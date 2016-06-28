5e487d498bff54eeeb204db0287af3ae44f8b5cc
Hoyn <369882070@qq.com>
2016-06-28 15:54:05 +0800

    Merge remote-tracking branch 'github/master'

---
+++ sample/src/main/java/com/sanniuben/circlerippleview/ui/MainActivity.java
@@ -0,0 +1,42 @@
+package com.sanniuben.circlerippleview.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.sanniuben.circlerippleview.R;
+import com.sanniuben.circlerippleview.view.WaveView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "btn", Toast.LENGTH_SHORT).show();
+            }
+        });
+        findViewById(R.id.btn_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "btn2", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        aa
+//
+//        ListView listView = (ListView) findViewById(R.id.listView);
+//        listView.setAdapter(new Adapter());
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(MainActivity.this, "listView "+position, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        WaveView waveView = (WaveView) findViewById(R.id.waveView);
+    }
+}
